92ec3147d70043796e33ed0a6fa040012a8ffb75
Ekubbo <45827702+Ekubbo@users.noreply.github.com>
2018-12-30 17:03:22 +0300

    Added display modal

--- static/static_files/js/home.js
+++ static/static_files/js/home.js
@@ -1,9 +1,15 @@
+Vue.component('modal', {
+	template: '#modal-template',
+});
+
 var app = new Vue({
 	delimiters: ["[[","]]"],
 	el: '#app',
 	data: {
+		selectedProduct: undefined,
 		products: [],
 		next_page: '/api/products/',
+		showModal: false
 	},
 	mounted: function () {
 		this.loadProducts();
@@ -11,14 +17,18 @@ var app = new Vue({
 	methods: {
 		loadProducts: function(event){
 			if (this.next_page === null) {
-				return
+				return;
 			}
-			
-			var that = this
+
+			var that = this;
 			axios.get(this.next_page).then(function(response){
 				that.products = that.products.concat(response.data['results']);
 				that.next_page = response.data['next'];
 			})
 		},
+		displayProduct: function (product) {
+			this.selectedProduct = product;
+			this.showModal = true;
+		},
 	}
-})
+});
